678039fcc8f08c6a33ee223927c91f99ba3cea3e
Mikolaj <mikolaj.og2002@gmail.com>
2020-04-08 10:30:05 +0200

    Not working onedirected list

--- List/Test/listTest.cpp
+++ List/Test/listTest.cpp
@@ -1,7 +1,15 @@
 #include <gtest/gtest.h>
 #include "list.hpp"
 #include <iostream>
-#include<string>
-#include <cstdint>
 
 using namespace ::testing;
+
+TEST(ListTest, SizeTest)
+{
+  List<int> list1;
+  list1.pushBack(1);
+  list1.pushBack(2);
+  list1.pushBack(3);
+  list1.print();
+  ASSERT_EQ(list1.size(), 3);
+}
--- main.cpp
+++ main.cpp
@@ -3,9 +3,5 @@
 
 int main()
 {
-  List<int> list1;
-  list1.pushBack(1);
-  list1.pushBack(2);
-  list1.pushBack(3);
-  list1.print();
+  return 0;
 }
