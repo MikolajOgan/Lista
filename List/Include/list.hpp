5465245a367fe0e23f12cee2f0264f52fae27647
Mikolaj <mikolaj.og2002@gmail.com>
2020-04-08 18:32:39 +0200

    working onedirected list, separated into divided files.

--- List/Include/list.hpp
+++ List/Include/list.hpp
@@ -1,23 +1,73 @@
-#pragma once
 #include <iostream>
 #include <cstdint>
-#include <cstdlib>
 
 template<typename type>
 
 class List
 {
-  public:
-    struct element
-    {
-      type value;
-      element *next;
+public:
+  struct element
+  {
+    type value;
+    element *next;
 
-      element(type value);
-    };
-    element *first;
-    List();
-    void print();
-    void pushBack(type value);
-    uint size();
+    element()
+    {
+        next = 0;
+    }
+  };
+
+  element *first;
+
+  void pushBack(type value)
+  {
+    element *new_element = new element;
+
+    new_element -> value = value;
+
+    if(first == 0)
+    {
+      first = new_element;
+    }
+    else
+    {
+      element *tmp = first;
+
+      while (tmp -> next) {
+          tmp = tmp -> next;
+      }
+
+      tmp -> next = new_element;
+      new_element -> next = 0;
+      }
+  }
+
+  void print()
+  {
+    element *tmp = first;
+
+    while(tmp)
+    {
+      std::cout << "value:" << tmp -> value;
+      tmp = tmp -> next;
+    }
+  }
+
+  int size()
+  {
+    element *tmp = first;
+    int size = 0;
+
+    while(tmp)
+    {
+      ++size;
+      tmp = tmp -> next;
+    }
+    return size;
+  }
+
+  List()
+  {
+    first = 0;
+  }
 };
